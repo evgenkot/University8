824ece50d1df0c014f4d231008ac648e844e5b2d
evgenkot <ukal.evgen@gmail.com>
2024-04-05 01:07:05 +0500

    CCB task_03 move vars to state

--- CCB/task_03/city-game/src/main.rs
+++ CCB/task_03/city-game/src/main.rs
@@ -8,23 +8,33 @@ pub enum GameStage {
 pub struct GameState {
     pub stage: GameStage,
     pub players: VecDeque<String>,
+    pub cities: HashSet<String>,
+    pub current_word: String,
+    pub last_char: char,
 }
 
-
 fn main() {
-    let mut state = GameState {stage: GameStage::Start, players: vec!["Player1".to_string(), "Player2".to_string()].into_iter().collect()};
-    let mut cities: HashSet<String> = HashSet::new();
-    let mut current_word = "abobus".to_string();
-    cities.insert(current_word.clone());
-    let mut last_char = current_word.trim().to_lowercase().chars().last().unwrap();
+    let mut state = GameState {
+        stage: GameStage::Start,
+        players: vec!["Player1".to_string(), "Player2".to_string()]
+            .into_iter()
+            .collect(),
+        cities: HashSet::new(),
+        current_word: "abobus".to_string(),
+        last_char: 's',
+    };
+    state.cities.insert(state.current_word.clone());
     loop {
         let mut guess = String::new();
         if state.players.len() <= 1 {
-            println!("Thats all {} won!", state.players.pop_front().unwrap_or("NO ONE".to_string()));
+            println!(
+                "Thats all {} won!",
+                state.players.pop_front().unwrap_or("NO ONE".to_string())
+            );
         }
         let current_player = state.players.pop_front().unwrap();
         println!("{} turn...", current_player);
-        println!("The current word is \"{}\"", current_word);
+        println!("The current word is \"{}\"", state.current_word);
 
         std::io::stdin()
             .read_line(&mut guess)
@@ -32,18 +42,18 @@ fn main() {
         guess = guess.trim().to_lowercase();
         let first_char = guess.chars().next().unwrap();
 
-        if first_char == last_char {
-            if cities.contains(&guess) {
+        if first_char == state.last_char {
+            if state.cities.contains(&guess) {
                 println!("{} already been guessed", guess);
                 state.players.push_front(current_player);
                 continue;
             }
-            current_word = guess.clone();
-            cities.insert(guess.clone());
-            last_char = guess.chars().last().unwrap();
+            state.current_word = guess.clone();
+            state.cities.insert(guess.clone());
+            state.last_char = guess.chars().last().unwrap();
             state.players.push_back(current_player);
         } else {
-            println!("{} not equal {}", last_char, first_char);
+            println!("{} not equal {}", state.last_char, first_char);
             state.players.push_front(current_player);
             continue;
         }
